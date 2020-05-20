a049f2b3085014c75287e2ee56a68abc2e3c1251
Gerry <55473965+gerrylwk@users.noreply.github.com>
2020-05-20 13:42:16 +0800

    Solve Ax=b where A = LU form

--- LUsolver.py
+++ LUsolver.py
@@ -13,11 +13,12 @@ def LUsolver(lu, b):                   # lu = b, where lu is a matrix consisting
         for j in range(i+1,n):
             x[i] -= lu[i][j] * x[j]
         x[i] /= lu[i][i]
+        x[i] = round(x[i],7)
     print(x)
     return(x)
 
 
-Atest = np.array([[1,2,0],[2,3,5],[4,1,1]])
-btest = np.array([5,23,9])
+Atest = np.array([[1,2,0],[2,3,5],[4,1,1]],dtype=float)
+btest = np.array([5,23,9],dtype=float)
 lutest = LUgauss(Atest)            #This function converts matrix Atest into LU form
 LUsolver(lutest,btest)             #This function prints out the values of x, i.e. = [1,2,3]
